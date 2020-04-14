1308c9f886722caf35969dd825cb5c9cd2fb6a7f
osystst <osystst@gmail.com>
2020-04-14 19:17:20 +0900

    JPA 실습

---
+++ src/main/java/kr/or/stew/service/posts/PostsService.java
@@ -0,0 +1,38 @@
+package kr.or.stew.service.posts;
+
+import kr.or.stew.domain.posts.Posts;
+import kr.or.stew.domain.posts.PostsRepository;
+import kr.or.stew.web.dto.PostsResponseDto;
+import kr.or.stew.web.dto.PostsSaveRequestDto;
+import kr.or.stew.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
