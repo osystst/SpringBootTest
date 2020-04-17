9ff125d1d85105a17f11a02d491ace944157393e
osystst <osystst@gmail.com>
2020-04-17 16:37:07 +0900

    구글 OAuth 개발 중

---
+++ src/main/java/kr/or/stew/config/auth/CustomOAuth2UserService.java
@@ -0,0 +1,52 @@
+package kr.or.stew.config.auth;
+
+import kr.or.stew.domain.user.User;
+import kr.or.stew.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        // 로그인 진행 중인 서비스 구분 코드
+        // 구글만 사용..
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        // OAuth2 로그인 키, Primary Key 의미
+        // 구글은 기본 제공 "sub"
+        // 네이버, 카카오는 선택 제공
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
+                .getUserInfoEndpoint().getUserNameAttributeName();
+
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+
+        User user = saveOrUpdate(attributes);
+
+        // 세션에 사용자 정보를 저장하기 위한 DTO
+        httpSession.setAttribute("user", new SessionUser(user));
+
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+                attributes.getAttributes(),
+                attributes.getNameAttributesKey());
+        )
+    }
+}
